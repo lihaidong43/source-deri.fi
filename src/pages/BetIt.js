33a63118cb8eed9b18a79da9580a8e026f32a3dd
huangwenchao2000 <76576293+huangwenchao2000@users.noreply.github.com>
2022-05-10 15:45:00 +0800

    fix

--- src/pages/BetIt.js
+++ src/pages/BetIt.js
@@ -1,12 +1,21 @@
 import Header from "../components/Header/Header";
 import './betit.scss'
-
 export default function BetIt({lang}){
   return (
     <div className="betit">
+      <div className='bg-img-color'>
+        
+      </div>
       <Header lang={lang}></Header>
       <div className="main-body">
-
+        <div className='title-des'>
+          <div className='title-text'>
+            {lang['title-one']}
+          </div>
+          <div className='title-text-des'>
+            {lang['title-two']}
+          </div>
+        </div>
       </div>
     </div>
   )
