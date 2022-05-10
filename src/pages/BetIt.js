96a0e634500eb96f24e80d5b42bf936124e36b26
lhd-dfactory <83153425+lhd-defactory@users.noreply.github.com>
2022-05-10 14:09:03 +0800

    support sass

--- src/pages/BetIt.js
+++ src/pages/BetIt.js
@@ -1,11 +1,12 @@
 import Header from "../components/Header/Header";
+import './betit.scss'
 
 export default function BetIt({lang}){
   return (
-    <div>
+    <div className="betit">
       <Header lang={lang}></Header>
       <div className="main-body">
-        
+
       </div>
     </div>
   )
