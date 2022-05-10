76998464b5b88d16db7787e7c8b96b0ddc5f115c
lhd-dfactory <83153425+lhd-defactory@users.noreply.github.com>
2022-05-10 17:13:53 +0800

    dev header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,14 +2,19 @@
 import WalletConnector from "../WalletConnector/WalletConnector";
 import styled  from 'styled-components';
 import BetIt from '../../pages/BetIt';
+import { useCallback, useEffect } from 'react';
+import classNames from "classnames";
+import { useState } from 'react';
+import { isElementInViewport } from '../../utils/utils';
 
 const Wrapper = styled.div`
   &.header {
+    position : initial;
     font-size :32px;
-    background : #FFAB00;
     display : flex;
     padding : 0 110px;
-    justify-content: space-between;
+    background : none;
+    justify-content: flex-start;
     align-items: center;
     height : 96px;
 
@@ -21,18 +26,52 @@ const Wrapper = styled.div`
     .l-name {
       font-weight: 800;
       color : #FFF;
+      margin-right :140px;
     }
+  }
+  &.header.fixed {
+    position : fixed;
+    top : 0px;
+    left : 0;
+    box-sizing: border-box;
+    width : 100%;
+    justify-content: space-between;
+    background : #FFAB00;
+    z-index : 10;
   }
 `
 
 export default function Header ({lang}) {
+  const [isFixed, setIsFixed] = useState(false);
+  const [btnMainColor, setBtnMainColor] = useState('#FFAB00');
+  const clazz = classNames('header',{
+    fixed : isFixed
+  })
+  const handler = useCallback(() => {
+    const st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+    if (st > 0){
+      setIsFixed(true)
+      setBtnMainColor('#FF7913')
+    } else {
+      setIsFixed(false)
+      setBtnMainColor('#FFAB00')
+    }
+  })
+
+  useEffect(() => {
+    document.addEventListener('scroll', handler, false);
+    return () => {
+      document.removeEventListener('scroll',handler)
+    }
+  },[])
+
   return (
-    <Wrapper className="header">
+    <Wrapper className={clazz}>
       <div className="left">
         <span className="f-name">{lang['bet'].toUpperCase()}</span><span className="l-name">{lang['it']}</span></div>
       <div className="right">
         {/* <NetworkSelector/> */}
-        <WalletConnector lang={lang}/>
+        <WalletConnector lang={lang} bgColor={btnMainColor}/>
       </div>
     </Wrapper>
   )
