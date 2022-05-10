e794a4968be88cc0e99e867f622a72569e96da98
lhd-dfactory <83153425+lhd-defactory@users.noreply.github.com>
2022-05-10 09:13:05 +0800

    setup

--- src/components/WalletConnector/WalletConnector.js
+++ src/components/WalletConnector/WalletConnector.js
@@ -1,6 +1,7 @@
 import { useCallback } from 'react';
 import { useWallet } from 'use-wallet';
 import Button from '../Button/Button';
+import { formatAddress } from '../../utils/utils';
 
 export default function WalletConnector({lang}){
   const wallet = useWallet()
@@ -9,7 +10,7 @@ export default function WalletConnector({lang}){
   },[wallet])
   return(
     <div>
-      <Button label={lang['connect-wallet']} onClick={connect}></Button>
+      <Button label={wallet.isConnected() ? formatAddress(wallet.account) : lang['connect-wallet']} onClick={connect}></Button> 
     </div>
   )
 }
