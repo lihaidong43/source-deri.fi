7597fea83d81fad51dd2aeca0f5c198628d8d9b5
huangwenchao2000 <76576293+huangwenchao2000@users.noreply.github.com>
2022-05-11 14:20:02 +0800

    fix

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,13 +1,30 @@
 import classNames from 'classnames'
 import Icon from '../Icon/Icon'
 import './card.scss'
-export default function Card({info,lang}){
-  return(
-    <div className={classNames('card-box',info.symbol)}>
+export default function Card({ info, lang }) {
+  return (
+    <div className={classNames('card-box', info.symbol)}>
       <div className='icon-name'>
         <Icon token={info.symbol} width={45} height={45} />
-        <span className={classNames('symbol-name',info.symbol)}>{info.symbol}</span> 
+        <span className='symbol-name'>{info.symbol}</span>
       </div>
+      <div className='price-box'>
+        <div className='symbol-price'>
+          ${info.price}
+        </div>
+        <div className='price-title'>
+          {lang['current-price']}
+        </div>
+      </div>
+      <div className=''>
+        <div className='symbol-leverage'>
+          {info.Leverage}
+        </div>
+        <div className='leverage-title'>
+          {lang['leverage']}
+        </div>
+      </div>
+
     </div>
   )
 }
