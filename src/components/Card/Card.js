05b7a78a628d171240b3dea9a81e3f04de5a34b7
huangwenchao2000 <76576293+huangwenchao2000@users.noreply.github.com>
2022-05-11 16:48:55 +0800

    fix

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,4 +1,5 @@
 import classNames from 'classnames'
+import Button from '../Button/Button'
 import Icon from '../Icon/Icon'
 import './card.scss'
 export default function Card({ info, lang }) {
@@ -16,13 +17,21 @@ export default function Card({ info, lang }) {
           {lang['current-price']}
         </div>
       </div>
-      <div className=''>
+      <div className='leverage-box'>
         <div className='symbol-leverage'>
           {info.Leverage}
         </div>
         <div className='leverage-title'>
           {lang['leverage']}  <Icon token="leverage" />
         </div>
+      </div>
+      <div className='input-box'>
+
+      </div>
+      <div className='btn-box'>
+        <Button label={lang['up']} className="btn" icon='up' width="299" height="60" bgColor="#38CB891A" hoverBgColor="#38CB89" borderSize={0} radius={14} fontColor="#38CB89" />
+        <Button label={lang['down']} className="btn" icon='down' width="299" height="60" bgColor="#FF56301A" hoverBgColor="#FF5630" borderSize={0} radius={14} fontColor="#FF5630" />
+        <Button label={lang['boosted-up']} className="btn" icon='boosted-up' width="299" height="60" bgColor="#FFAB001A" hoverBgColor="#FFAB00" borderSize={0} radius={14} fontColor="#FFAB00" />
       </div>
 
     </div>
